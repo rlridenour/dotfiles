cc0f3bec7aec41f74039c770454a115da325319d
Randy Ridenour <rlridenour@gmail.com>
2012-07-13 09:17:46 -0500

    Added packages to init.el

--- init.el
+++ init.el
@@ -11,3 +11,60 @@
      (eval-print-last-sexp)))))
 
 
+;; set local recipes
+(setq
+ el-get-sources
+ '((:name buffer-move			; have to add your own keys
+	  :after (lambda ()
+		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
+		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
+		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
+		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
+
+   (:name smex				; a better (ido like) M-x
+	  :after (lambda ()
+		   (setq smex-save-file "~/.emacs.d/.smex-items")
+		   (global-set-key (kbd "M-x") 'smex)
+		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
+
+   (:name magit				; git meet emacs, and a binding
+	  :after (lambda ()
+		   (global-set-key (kbd "C-x C-z") 'magit-status)))
+
+   (:name goto-last-change		; move pointer back to last change
+	  :after (lambda ()
+		   ;; when using AZERTY keyboard, consider C-x C-_
+		   (global-set-key (kbd "C-x C-/") 'goto-last-change)))))
+
+;; now set our own packages
+(setq
+ my:el-get-packages
+ '(el-get				; el-get is self-hosting
+   markdown-mode                        ; mode for editing markdown-files
+   textmate
+   escreen            			; screen for emacs, C-\ C-h
+   php-mode-improved			; if you're into php...
+   switch-window			; takes over C-x o
+  ;; auto-complete			; complete as you type with overlays
+   zenburn-theme))	                ; check out color-theme-solarized
+
+;;
+;; Some recipes require extra tools to be installed
+;;
+;; Note: el-get-install requires git, so we know we have at least that.
+;;
+
+
+(when (el-get-executable-find "svn")
+  (loop for p in '(psvn    		; M-x svn-status
+		   yasnippet		; powerful snippet mode
+		   )
+	do (add-to-list 'my:el-get-packages p)))
+
+(setq my:el-get-packages
+      (append
+       my:el-get-packages
+       (loop for src in el-get-sources collect (el-get-source-name src))))
+
+;; install new packages and init already installed packages
+(el-get 'sync my:el-get-packages)
