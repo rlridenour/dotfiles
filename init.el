19b87920a1756a244740559ecb80fe77b501e2a9
Randy Ridenour <rlridenour@gmail.com>
2013-02-15 20:01:34 -0600

    Added newline above and below
    Super-j for newline-below, and super-k for newline-above.

--- init.el
+++ init.el
@@ -162,6 +162,24 @@
 ;; use abbrev-mode
 (setq default-abbrev-mode t)
 
+;; From https://github.com/defunkt/textmate.el
+(defun newline-next ()
+  "Inserts an indented newline after the current line and moves the point to it."
+  (interactive)
+  (end-of-line)
+  (newline-and-indent))
+  (global-set-key (kbd "s-j") 'newline-next)
+
+
+(defun newline-previous ()
+  "Insert a blank line above the cursor and move the cursor up one line."
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (previous-line)
+  (indent-according-to-mode))
+  (global-set-key (kbd  "s-k") 'newline-previous)
+
 
 ;; Org-mode suggested key bindings
 (global-set-key "\C-cl" 'org-store-link)
